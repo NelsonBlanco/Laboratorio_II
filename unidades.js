2f0a5ba618347407ab678455892b788cf06e4805
NelsonBlanco <89229615+NelsonBlanco@users.noreply.github.com>
2022-02-13 21:19:23 -0600

    Add files via upload

---
+++ unidades.js
@@ -0,0 +1,31 @@
+var unidades = {
+    'Longitud': {
+        'grupo': 'Longitud',
+        'unidades': [
+            'kilometro',
+            'metro',
+            'centimetro',
+            'milimetro',
+            'micrometro',
+            'nanometro',
+            'milla',
+            'yarda',
+            'pie',
+            'pulgada'
+        ],
+        'equivalencias': {
+            'kilometro': {
+                'kilometro': 1,
+                'metro': 1000,
+                'centimetro': 100000,
+                'milimetro': 1e+6,
+                'micrometro': 1e+9,
+                'nanometro': 1e+12,
+                'milla': 0.621371,
+                'yarda': 1093.61,
+                'pie': 3280.84,
+                'pulgada': 39370.1
+            },
+        },
+    },
+},
